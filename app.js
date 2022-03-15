20a682d0eac8c677892d34780fe497d4a2029b2e
Daniel <karczmarczykdaniel@gmail.com>
2022-03-15 16:42:22 +0100

    basic countdown

--- app.js
+++ app.js
@@ -12,17 +12,35 @@ class Timer {
 
 		this.playButton.addEventListener('click', this.start);
 		this.pauseButton.addEventListener('click', this.pause);
+		this.inType.value = '0';
 	}
 
 	start = () => {
 		console.log('START', this);
+		if (this.timerDur < 0) return;
+		this.tick;
+		this.interval = setInterval(() => {
+			this.tick();
+		}, 1000);
 	};
 	pause = () => {
 		console.log('pause', this);
+		clearInterval(this.interval);
 	};
-	timervalue = () => {
-		let time = inType.getAttribute('value');
+	tick = () => {
+		//let time = inType.getAttribute('value');
+		//	console.log('tick');
+		if (this.timerDur < 0) return clearInterval(this.interval);
+		this.timerDur = this.timerDur - 1;
 	};
+
+	get timerDur() {
+		//console.log(this.inType.value);
+		return parseFloat(this.inType.value);
+	}
+	set timerDur(time) {
+		this.inType.value = time;
+	}
 }
 
 const timer = new Timer(inType, playButton, pauseButton);
