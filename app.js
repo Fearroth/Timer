beb53919b530712770718c8ee2df28fef8a989fb
Daniel <karczmarczykdaniel@gmail.com>
2022-03-16 12:42:33 +0100

    atransform and stroke scalable vector graphic

--- app.js
+++ app.js
@@ -3,57 +3,14 @@ console.log('test');
 const inType = document.querySelector('#inputType');
 const playButton = document.querySelector('#playButton');
 const pauseButton = document.querySelector('#pauseButton');
+const circle = document.querySelector('circle');
 
-class Timer {
-	constructor(inType, playButton, pauseButton, callbacks) {
-		this.inType = inType;
-		this.playButton = playButton;
-		this.pauseButton = pauseButton;
-
-		this.playButton.addEventListener('click', this.start);
-		this.pauseButton.addEventListener('click', this.pause);
-		this.inType.value = '0';
-
-		if (callbacks) {
-			this.onTick = callbacks.onTick;
-			this.onStart = callbacks.onStart;
-			this.onPause = callbacks.onPause;
-		}
-	}
-
-	start = () => {
-		console.log('START', this);
-		this.onStart();
-		this.tick;
-		this.interval = setInterval(() => {
-			this.tick();
-		}, 1000);
-	};
-	pause = () => {
-		this.onPause();
-		console.log('pause', this);
-		clearInterval(this.interval);
-	};
-	tick = () => {
-		//let time = inType.getAttribute('value');
-		//	console.log('tick');
-		if (this.timerDur <= 0) return clearInterval(this.interval);
-		this.onTick();
-		this.timerDur = this.timerDur - 1;
-	};
-
-	get timerDur() {
-		//console.log(this.inType.value);
-		return parseFloat(this.inType.value);
-	}
-	set timerDur(time) {
-		this.inType.value = time;
-	}
-}
-
+//circle.setAttribute('stroke-dashoffset', '200');
 const timer = new Timer(inType, playButton, pauseButton, {
 	onTick() {
 		console.log('1');
+		const off = parseInt(circle.getAttribute('stroke-dashoffset')) - 1;
+		circle.setAttribute('stroke-dashoffset', off);
 	},
 	onStart() {
 		console.log('2');
@@ -62,3 +19,5 @@ const timer = new Timer(inType, playButton, pauseButton, {
 		console.log('3');
 	}
 });
+
+//console.log(timer.inType.value);
---
+++ timer.js
@@ -0,0 +1,46 @@
+class Timer {
+	constructor(inType, playButton, pauseButton, callbacks) {
+		this.inType = inType;
+		this.playButton = playButton;
+		this.pauseButton = pauseButton;
+
+		this.playButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
+		this.inType.value = '0';
+
+		if (callbacks) {
+			this.onTick = callbacks.onTick;
+			this.onStart = callbacks.onStart;
+			this.onPause = callbacks.onPause;
+		}
+	}
+
+	start = () => {
+		console.log('START', this);
+		this.onStart();
+		this.tick;
+		this.interval = setInterval(() => {
+			this.tick();
+		}, 1000);
+	};
+	pause = () => {
+		this.onPause();
+		console.log('pause', this);
+		clearInterval(this.interval);
+	};
+	tick = () => {
+		//let time = inType.getAttribute('value');
+		//	console.log('tick');
+		if (this.timerDur <= 0) return clearInterval(this.interval);
+		this.onTick();
+		this.timerDur = this.timerDur - 1;
+	};
+
+	get timerDur() {
+		//console.log(this.inType.value);
+		return parseFloat(this.inType.value);
+	}
+	set timerDur(time) {
+		this.inType.value = time;
+	}
+}
